21f0398abca4f3a5760054b205b0aceebd22829b
Shea Sullivan <sheasullivan@sheas-mbp.home>
2023-12-02 15:31:49 -0400

    Day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,43 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def parse_input(input) do
+    String.split(input, "\n", trim: true) |>
+    Map.new(fn line ->
+      ["Game " <> id | sets] = String.split(line, [": ", "; "])
+      sets = sets
+      |> Enum.map(fn set -> 
+          String.split(set, [" ", ", "])
+          |> Enum.chunk_every(2)
+          |> Map.new(fn [number, colour] -> {colour, String.to_integer(number)} end) 
+        end)
+      # Merge all rounds together to just have the max
+      |> Enum.reduce(
+          %{"red" => 0, "green" => 0, "blue" => 0},
+          &Map.merge(&1, &2, fn _colour, count1, count2 -> max(count1, count2) end)
+        )
+      {String.to_integer(id), sets}
+    end)
+  end
+  def part1(args) do
+    total_red = 12
+    total_blue = 14
+    total_green = 13
+    
+    args
+    |> parse_input()
+    |> Map.filter(fn { _gameId, maxDrawn } -> 
+      maxDrawn["red"] <= total_red and maxDrawn["green"] <= total_green and maxDrawn["blue"] <= total_blue
+    end)
+    |> Map.keys()
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> parse_input()
+    |> Enum.map(fn {_gameId, maxDrawn} ->
+      Map.values(maxDrawn)
+      |> Enum.product()
+    end)
+    |> Enum.sum()
   end
 end
