0fb4a1c87a725fb6e13ad3eb4a9a9c21d24c6b52
Shea Sullivan <sheasullivan@sheas-mbp.home>
2023-12-06 21:16:15 -0400

    Day4 pt1

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,25 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def parseInput(input) do
+    String.split(input, "\n", trim: true)
+    |> Map.new(fn line ->
+      ["Card " <> id | numbers] = String.split(line, [": ", " | "])
+      [owned, winners] = numbers
+      owned = MapSet.new(String.split(owned, " ", trim: true))
+      winners = MapSet.new(String.split(winners, " ", trim: true))
+      owned_winners = MapSet.intersection(owned, winners)
+      |> Enum.uniq()
+      {String.to_integer(String.trim(id)), owned_winners}
+    end)
+  end
+  def part1(args) do
+    parseInput(args)
+    |> Enum.map(fn {_id, winning_numbers}-> 
+      case length(winning_numbers) do
+        0 -> 0
+        length -> round(:math.pow(2, length-1))
+      end
+    end)
+    |> Enum.sum()
   end
 
   def part2(_args) do
